08b84c589d437cc227a048309049315d00298b01
Anil <anilmende2002@email.com>
2025-09-15 18:08:02 +0530

    v8

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
-import './Navbar.css';
 import { assets } from "../../assets/assets";
+import styled from "styled-components";
 const Navbar = () => {
 
     const [menu, setMenu] = useState("home");
 
     const menuItems = [
         {
-            name:"Home",
-            type:"home"
+            name: "Home",
+            type: "home"
         },
         {
-            name:"Menu",
-            type:"menu"
+            name: "Menu",
+            type: "menu"
         },
         {
-            name:"Mobile-App",
-            type:"mobile-app"
+            name: "Mobile-App",
+            type: "mobile-app"
         },
         {
-            name:"Contact-Us",
-            type:"contact-us"
+            name: "Contact-Us",
+            type: "contact-us"
         }
     ]
 
-    return(
-        <div className="navbar">
+    return (
+        <NavbarContainer id="navbar">
             <img src={assets.logo} alt="" className="logo" />
 
             <ul className="navbar-menu">
@@ -52,7 +52,71 @@ const Navbar = () => {
 
                 <button>Sign In</button>
             </div>
-        </div>
+        </NavbarContainer>
     )
 }
 export default Navbar;
+
+const NavbarContainer = styled.div`
+      padding: 20px 0px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .logo{
+        width: 150px;
+      }
+
+      .navbar-menu{
+        display: flex;
+        list-style: none;
+        gap: 20px;
+        font-size: 18px;
+        color: rgb(91, 91, 191);
+     }
+
+     .navbar-right{
+       display: flex;
+       align-items: center;
+       gap: 40px;
+     }
+
+     .navbar-right button{
+      background: transparent;
+      font-size: 16px;
+      color: #49557e;
+      border: 1px solid tomato;
+      padding: 10px 30px;
+      border-radius: 50px;
+      cursor: pointer;
+      transition: 0.3s;
+     }
+
+     .navbar-right button:hover{
+       background-color: #fff4f2;
+     }
+
+     .active{
+       padding-bottom: 2px;
+       border-bottom: 2px solid #49557e;
+     }
+
+     li{
+       cursor: pointer;
+     }
+
+     .navbar-search-icon{
+       position: relative;
+     }
+
+     .navbar-search-icon .dot{
+      position: absolute;
+      min-width: 10px;
+      min-height: 10px;
+      background-color: tomato;
+      border-radius: 5px;
+      top: -8px;
+      right: -8px;
+     }
+     
+`;
